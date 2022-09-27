c9fdb4bcfb078ee0055d5ad6df8ed9cafbe14526
coder-wjh <69297552+coder-wjh@users.noreply.github.com>
2022-09-27 23:58:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def get_access_token():
 def get_weather(region):
     region_url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + region
     response = get(region_url).json()
-    data = = response['data']['list'][0]
+    data = response['data']['list'][0]
     # 天气
     weather = data['weather']
     # 当前温度
